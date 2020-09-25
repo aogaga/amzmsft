8d4471f2f403009b5aa3d53cc7ae74831f6d0f96
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2020-09-25 00:11:04 -0500

    moving to lappy

--- src/main/java/questions/App.java
+++ src/main/java/questions/App.java
@@ -4,26 +4,40 @@ import java.util.HashMap;
 
 public class App {
 
+    public static void main(String [] args){
+        System.out.println(fib(3, 4));
+    }
     public static int fib(int n, int k){
-        if(k < n){
+        if(k <= n){
             return 1;
         }
          int result  = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < k; i++){
-
-            if(i < n){
-                map.put(i, i);
-            }
-
+        for(int i = 0; i <= k; i++){
             int total = 0;
+            if(i < n){
+                total = 1;
+            }else{
 
-            for(int z = 1; z < n; z++){
-                
+
+
+                for(int z = n; z > 0; z--){
+
+                    total = total + map.get(i-z);
+                }
+
+
             }
+
+            map.put(i, total);
         }
 
-        return result;
+
+//        for(int i = 0; i <= k; i++){
+//            System.out.println(i);
+//           System.out.println(map.get(i));
+//        }
+        return map.get(k);
     }
 }
